a417de8b1977f148da89f18e0d7a98b9e470e94f
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-11-14 18:47:24 +0530

    Image from Assets using CircleAvatar Widget, Name and Title Text Widget - Basic UI Created

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,38 @@ class MyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            child: const Text("Hello"),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('assets/images/ironcat.jpg'),
+                ),
+                Text(
+                  'Sahil Rajput',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    letterSpacing: 5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Java Developer',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
